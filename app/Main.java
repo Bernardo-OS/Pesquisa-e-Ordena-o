49b4d460b55c37b55f9d188d11e5eeef97ada9e1
BERNARDO DE OLIVEIRA DOS SANTOS <85496116+Bernardo-OS@users.noreply.github.com>
2026-08-20 19:58:17 -0300

    Ordenação em java utilizando orgaização MVC

---
+++ app/Main.java
@@ -0,0 +1,8 @@
+import app.controller.ProcessoController;
+
+public class Main{
+    public static void main(String[] args) {
+        ProcessoService service = new ProcessoController;
+        Processo
+    }
+}
